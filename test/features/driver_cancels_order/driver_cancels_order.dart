fef4e1685eae62f0f5ac6fc16f1090a1fa42f529
Anwar Tuha <anwartuha2@gmail.com>
2022-11-17 20:15:54 +0300

    <feat>: DN-324_Driver_cancels_order - Added bloc test to test for state changes when cancelling an order is initiated - The states are OrderCancelled, OrderCancellationFailed, CancellationReasonsHere, CancellationReasonsFailed

---
+++ test/features/driver_cancels_order/driver_cancels_order.dart
@@ -0,0 +1,20 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:lastmile_mobile/src/data/datasources/local/app_hive_service.dart';
+import 'package:lastmile_mobile/src/injector.dart';
+import 'package:lastmile_mobile/src/presentation/views/home_page/blocs/order/order_bloc.dart';
+
+import '../../utils/test_injector.dart';
+
+Future<void> main() async {
+  await AppHiveService.instance.initHiveBoxes();
+  group('''Driver cancels order''', () {
+    initializeTestDependencies();
+    blocTest(
+      'Driver cancels order',
+      build: () => OrderBloc(injector()),
+      act: (bloc) => bloc.add(const CancelOrderEvent('', '', '')),
+      expect: () => [const OrderCancelled(), const OrderUnassigned()],
+    );
+  });
+}
